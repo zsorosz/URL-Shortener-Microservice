8de2fce48a302a7fe7ea077e6732a6092c452b56
zsorosz <oroszi.zsofi@gmail.com>
2023-11-06 13:14:19 +0100

    Validate url input

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+const url = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// parse application/json
+app.use(bodyParser.json());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  const urlInput = req.body.url;
+  const parsedUrl = url.parse(urlInput);
+  dns.lookup(parsedUrl.hostname, (err, addresses) => {
+    if (err || addresses === undefined) {
+      res.json({ error: "invalid url" });
+    } else {
+      res.json({ original_url: req.body.url });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
